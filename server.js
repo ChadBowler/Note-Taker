c3c5174acefbe091b07163484a1fa00da6d987c3
Chad Bowler <chadbowler@gmail.com>
2023-06-24 21:55:05 -0600

    added route for index file to check deployment

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
-
-const express = require('express')
+const path = require('path');
+const express = require('express');
 const app = express();
+app.use(express.static('Develop'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
+})
 
 app.listen(process.env.PORT || 3000);
