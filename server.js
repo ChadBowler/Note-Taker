325e5498480c146bcd3dc045ea6b09edbe2334c0
Chad Bowler <chadbowler@gmail.com>
2023-06-24 21:35:02 -0600

    installed dotenv and added port for listening

---
+++ server.js
@@ -0,0 +1,8 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express')
+const app = express();
+
+app.listen(process.env.PORT || 3000);
