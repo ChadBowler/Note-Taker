bd48f4d0e328614d1cd03305298654bb874b3fbc
Chad Bowler <chadbowler@gmail.com>
2023-06-24 22:25:41 -0600

    added more routes

--- server.js
+++ server.js
@@ -4,10 +4,23 @@ if (process.env.NODE_ENV !== 'production') {
 const path = require('path');
 const express = require('express');
 const app = express();
-app.use(express.static('Develop'));
+const { v4: uuidv4 } = require('uuid');
+// app.use(express.static(path.join(__dirname, 'Develop')));
+
+// console.log(__dirname);
+
+//routes
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
-})
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
+});
 
 app.listen(process.env.PORT || 3000);
